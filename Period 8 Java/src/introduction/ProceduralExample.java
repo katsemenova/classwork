bf8afc0888444838147349c8b7aaa3bfe4860022
Kat Semenova <katsemenova@users.noreply.github.com>
2016-09-15 13:51:38 -0400

    Iteration through 2 arrays

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -3,6 +3,9 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[] args){
 	  String[] names={"Jillian","Joseph","Jillian"};
-	  System.out.println("Hi, my name is " + names[0]);
+	  String[] years={"Freshman","Sophmore","Senior"};
+	  for (int i=0; i<3;i++){
+	  System.out.println("Hi, my name is " + names[i] +" and I am a " + years[i]);
+  }
   }
 }
