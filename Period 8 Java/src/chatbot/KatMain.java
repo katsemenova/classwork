5f5b3e42c4646739d12168ee087ab3b3f2a61179
Student 8 <Student 8@10.8.33.163>
2016-09-27 14:02:08 -0400

    No negations added

--- Period 8 Java/src/chatbot/KatMain.java
+++ Period 8 Java/src/chatbot/KatMain.java
@@ -47,25 +47,54 @@ public class KatMain {
 		keyword=keyword.toLowerCase();
 		//find first position of key word
 		int psn=searchString.indexOf(keyword);
+		System.out.println("keyword found at "+psn);
+		System.out.println("string is "+searchString);
 		//keep searching until keyword found
 		while (psn>=0){
-			//assume preceeded and followed by space
+			//assume preceded and followed by space
 			String before=" ";
 			String after=" ";
-			//check if character in front e=of it exists
+			//check if character in front of it exists
 			if(psn>0){
 				before=searchString.substring(psn-1, psn);
 			}
 			//check for characted after keyword
-			if(pns+keyword.length()<searchString.length()){
+			if(psn+keyword.length()<searchString.length()){
 				after=searchString.substring(psn+keyword.length(),psn+keyword.length()+1);
 			}
-			if(before.compareTo("a")<0 && after.compareTo("a"))
+			if((before.compareTo("a")<0) && (after.compareTo("a")<0)&&noNegations(searchString,psn)){				noNegations(searchString,psn);
 				return psn;
+			}
 			else
-				psn=searchString.indexOf(keyword,psn+1)
-		}
+				psn=searchString.indexOf(keyword,psn+1);
+		}                                                      
 		return -1;
+	}
+	/**helper method thats assigned for helping a larger method, generally they are private because they
+	 *are only used by the methods they are helping
+	 *@param search string
+	 *@param psn
+	 * @return 
+	 *@return "true" if no negation words
+	 */
+	private static boolean noNegations(String searchString, int psn) {
+		//check to see if the word "no" is in front of psn
+		//check to see if 3 spaces in front then check if no is there
+		if(psn-3>=0&&searchString.substring(psn-3,psn).equals("no ")){
+			return false;
+		}
+		//check for not
+		if(psn-6>=0&&searchString.substring(psn-3,psn).equals("never ")){
+			return false;
+		}
+		if(psn-4>=0&&searchString.substring(psn-3,psn).equals("not ")){
+			return false;
+		}
+		if(psn-4>=0&&searchString.substring(psn-3,psn).equals("n't ")){
+			return false;
+		}
+		return true;
+		
 	}
 	public static String promptInput(){
 		String userInput = input.nextLine();
