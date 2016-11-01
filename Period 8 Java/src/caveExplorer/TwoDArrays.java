46ac36aee76912343d9485521668f654ef1628e8
Student 8 <Student 8@10.8.33.163>
2016-11-01 14:04:49 -0400

    s

--- Period 8 Java/src/caveExplorer/TwoDArrays.java
+++ Period 8 Java/src/caveExplorer/TwoDArrays.java
@@ -5,6 +5,38 @@ import java.util.Arrays;
 public class TwoDArrays {
 
 	public static void main(String[] args) {
+
+		String[][] pic = new String[10][8];
+		for(int row=0;row<pic.length;row++){
+			//populate with coordinates
+			for( int col=0;col<pic[row].length;col++){
+				pic[row][col]=" ";
+			}
+		}
+		//grassy field
+		for(int row=5;row<pic.length;row++){
+			for(int col=0;col<pic[row].length;col++){
+				pic[row][col]="w";
+						
+			}
+		}
+		
+		pic[0][4]=" |";
+		pic[1][4]="O";
+		pic[2][4]=" |";
+		pic[1][3]="--";
+		pic[1][5]="--";
+		pic[0][1]="<";
+		pic[1][0]="<";
+		pic[2][1]="<";
+		for(String[] row:pic){
+			for(String col:row){
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+	}
+	public static void firstLesson(){
 		String[] xox = {"x","o","x","o","x"};
 		//a 1D array prints out a horizontal string
 		System.out.println(Arrays.toString(xox));
