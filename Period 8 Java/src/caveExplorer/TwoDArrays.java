eda32ae22738cede0cb3151f0fbe0b74a749902b
Student 8 <Student 8@10.8.33.163>
2016-11-02 14:02:26 -0400

    s

--- Period 8 Java/src/caveExplorer/TwoDArrays.java
+++ Period 8 Java/src/caveExplorer/TwoDArrays.java
@@ -6,6 +6,65 @@ public class TwoDArrays {
 
 	public static void main(String[] args) {
 
+		boolean[][] mines = new boolean[6][6];
+		plantMines(mines);
+		String[][] field = createField();
+		print(field);
+	}
+	private static String[][] createField(boolean[][] mines) {
+		String[][] field = new String[mines.length][mines[0].length];
+		
+		for(int row=0;row<field.length;i++){
+			for(int col=0;col<field[row].length;col++){
+				if(mines[row][col])
+					field[row][col] ="x";
+				else{
+					field[row][col]=countNearby(mines,row,col);
+				}
+			}
+		}
+		return field;
+	}
+	private static String countNearby(boolean[][] mines, int row, int col) {
+		for(int r=row-1;r<=row+1;r++){
+			for(int c=col-1;c<=col+1;c++){
+				//to check that the element exists
+				if(r>=0&&r<mines.length&&c>=0&&c<mines[0].length){
+					
+				}
+			}
+		}
+		return null;
+	}
+	private static void plantMines(boolean[][] mines) {
+		int numberOfMines = 10;
+		while(numberOfMines>0){
+			int row = (int)(Math.random()*mines.length);
+			int col = (int) (Math.random()*mines[0].length);
+			//this prevents the same mine being selected twice
+//			while(mines[row][col]){
+//				row = (int)(Math.random()*mines.length);
+//				col = (int) (Math.random()*mines[0].length);
+//			}
+				
+			if(!mines[row][col]){
+				mines[row][col]=true;
+				numberOfMines--;
+			}
+		}
+		
+	}
+	public static void print(String[][] pic){
+		for(String[] row:pic){
+			for(String col:row){
+				
+				System.out.print(col);
+			}
+			System.out.println();
+		}
+	}
+	public static void makePicture(){
+
 		String[][] pic = new String[10][8];
 		for(int row=0;row<pic.length;row++){
 			//populate with coordinates
@@ -20,17 +79,21 @@ public class TwoDArrays {
 						
 			}
 		}
+		//top and bottom row should be "_"
+		for(int col=0;col<pic[0].length;col++){
+			pic[0][col]="_";
+			pic[pic.length-1][col]="_";
+		}
 		
-		pic[0][4]=" |";
-		pic[1][4]="O";
-		pic[2][4]=" |";
-		pic[1][3]="--";
-		pic[1][5]="--";
-		pic[0][1]="<";
-		pic[1][0]="<";
-		pic[2][1]="<";
+		for(int row =1; row<pic.length;row++){
+			pic[row][0]="|";
+			pic[row][pic[row].length-1]="|";
+		}
+			
+		
 		for(String[] row:pic){
 			for(String col:row){
+				
 				System.out.print(col);
 			}
 			System.out.println();
