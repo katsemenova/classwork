1478e44fbf3836e8a328eb9438e024fb98b59b6f
Kat Semenova <katya.semenova1405@gmail.com>
2017-05-12 13:30:26 -0400

    transfer

---
+++ Period 8 Java/src/game/mainScreenTeam/HomeScreen.java
@@ -0,0 +1,60 @@
+package game.mainScreenTeam;
+
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import guiPractice.ClickableScreen;
+import guiPractice.components.Graphic;
+import guiPractice.components.Visible;
+
+
+/**
+ * @author Kat 
+ * @author Jenniber
+ *
+ */
+public class HomeScreen extends ClickableScreen implements Runnable{
+
+	private Graphic background;
+	public static HomeJenniber jenCode;
+
+	public HomeScreen(int width, int height) {
+		super(width, height);
+		Thread play = new Thread(this);
+		play.start();
+		
+	}
+
+	@Override
+	public void initAllObjects(ArrayList<Visible> viewObjects) {
+
+		background=new Graphic(0,0,getWidth(),getHeight(),"img/Grassland.jpg");
+		viewObjects.add(background);
+		HomeKat katCode=new HomeKat(viewObjects, getWidth(), getHeight());
+		
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		Draggable d= (Draggable) draggables.get(0);
+		if(d.isHovered(e.getX(), e.getY()) || alreadyDragging){
+			alreadyDragging = true;
+			d.setX(e.getX() - 37);
+			d.setY(e.getY() - 37);
+		}
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		Draggable d= (Draggable) draggables.get(0);
+		jenCode.checkFeed(e.getX(),e.getY());
+		d.setX(d.getOrigX());
+		d.setY(d.getOrigY());
+		alreadyDragging=false;
+		
+	}
+	@Override
+	public void run() {
+	}
+
+}
